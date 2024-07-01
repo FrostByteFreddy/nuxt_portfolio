b470bd43812f308b58d6a9840d1e5598c88ea237
Moritz Burn <moritzburn@Moritzs-MacBook-Air.local>
2024-07-01 08:35:14 +0200

    Layouts actually working+

---
+++ components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h3>Footer</h3>
+          <p>Your company information, address, etc. can go here.</p>
+        </div>
+        <div class="col-md-3">
+          <h4>Links</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h4>Social</h4>
+          <ul class="list-unstyled">
+            <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
+            <li><a href="#"><i class="fab fa-facebook"></i> Facebook</a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr>
+      <p class="text-center">&copy; 2023 Your Company</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <nuxt-link to="/" class="navbar-brand">Your Brand</nuxt-link>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li v-for="link in links" :key="link.name" class="nav-item">
+            <nuxt-link :to="link.to" class="nav-link" exact-active-class="active">
+              {{ link.name }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      open: false,
+      links: [
+        { to: '/', name: 'Home' },
+        { to: '/about', name: 'About' },
+        { to: '/contact', name: 'Contact' },
+        { to: '/experience', name: 'Experience' },
+        { to: '/work', name: 'Work' },
+      ]
+    }
+  },
+  watch: {
+    '$route.name' () {
+      this.open = false
+    }
+  }
+}
+</script>
