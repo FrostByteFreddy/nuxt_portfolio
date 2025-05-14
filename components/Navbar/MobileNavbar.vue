3e56acb69dade49d1a3ba9300db36178b27d433e
Moritz Burn <moritzburn@Moritzs-MacBook-Air.local>
2025-05-14 15:25:55 +0200

    Added g-x classes to space rows

--- components/Navbar/MobileNavbar.vue
+++ components/Navbar/MobileNavbar.vue
@@ -1,24 +1,24 @@
 <template>
-    <div data-device="mobile" :class="{ active: isMenuHidden }">
+    <div data-device="mobile" :class="{ active: !isMenuHidden }">
         <a href="#" class="nav-logo">
-            <img class="nav-logo-img" src="https://symbols.getvecta.com/stencil_25/77_sass.57898c574e.svg"
+            <img class="nav-logo-img" src="~/static/images/logo_example.svg"
                 alt="SASS Logo">
         </a>
 
-        <a id="burger-menu" @click="toggleMenu">
+        <a id="burger-menu" @click="toggleMenu" role="button" aria-expanded="isMenuHidden ? 'false' : 'true'" aria-controls="mobile-menu-list">
             <span data-id="1" class="burger-menu-bar"></span>
             <span data-id="2" class="burger-menu-bar"></span>
             <span data-id="3" class="burger-menu-bar"></span>
         </a>
 
-        <ul class="nav-menu-links-mobile">
+        <ul class="nav-menu-links-mobile" id="mobile-menu-list">
             <li v-for="link in links" :key="link.name" class="nav-menu-link">
                 <nuxt-link :to="link.to" class="nav-link" exact-active-class="current">
                     {{ link.name }}
                 </nuxt-link>
             </li>
             <li class="nav-menu-link">
-                <a class="btn primary">Contact</a>
+                <a href="#" class="btn primary">Contact</a>
             </li>
         </ul>
     </div>
@@ -26,10 +26,12 @@
 
 <script>
 export default {
+    name: 'MobileNavigation',
     props: {
         links: {
             type: Array,
             required: true,
+            default: () => [],
         },
     },
     data() {
