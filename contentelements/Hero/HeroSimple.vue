4ad71188df439674055e6e2738704f1c68f055c7
Moritz Burn <moritzburn@moritzs-macbook-air.home>
2025-05-15 16:23:52 +0200

    Gradient for hero image

--- contentelements/Hero/HeroSimple.vue
+++ contentelements/Hero/HeroSimple.vue
@@ -1,13 +1,14 @@
 <template>
-    <section data-component="hero" class="background-black hero-simple" :style="{ 'background-image': `url(${backgroundImage})` }">
+    <!-- <section data-component="hero" class="background-black hero-simple" :style="{ 'background-image': `url(${backgroundImage})` }"> -->
+    <section data-component="hero" class="background-black hero-simple">
         <div class="container">
             <div class="row g-5">
-                <div class="col-md-6 col-12">
+                <div class="col-md-6 col-12 align-center">
                     <div v-html="title"></div>
                     <div v-html="text"></div>
                     <a v-if="buttonText && buttonUrl" :href="buttonUrl" :class="buttonClasses">{{ buttonText }}</a>
                 </div>
-                <div class="col-md-6 col-12">
+                <div class="col-md-6 col-12 align-center">
                     <img alt="" :src=heroImage />
                 </div>
             </div>
